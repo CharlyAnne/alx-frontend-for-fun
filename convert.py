960fd42e7b6b640c111c0b2b234b4df7203b57c2
CharlyAnne <icharlieanne@gmail.com>
2023-07-11 22:18:42 +0100

    generated html

--- convert.py
+++ convert.py
@@ -2,7 +2,8 @@ import markdown2
 
 with open('test.md', 'r') as f:
     text = f.read()
-    html = markdown2.markdown(text)
+    
+html = markdown2.markdown(text)
 
-with open('test.md', 'w') as f:
+with open('test.html', 'w') as f:
     f.write(html)
